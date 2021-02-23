6909f1baf423506e4c43a6a029723f3875ca43b5
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-23 10:13:40 +0530

    merkle tree function added

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,10 @@
 from utils import hash
+
 class CryptoChain:
 	def __init__(self):
 		self.blocks=[]
-		self.genesis_block=create_block(1)
 
-	def create_block(self,index=1,transactions,nounce,prevoius_hash,merkle_root):
+	def create_block(self,index,transactions,nounce,prevoius_hash,merkle_root):
 		string=str(index)+str(transactions)+str(nounce)+str(prevoius_hash)+str(merkle_root)
 		current_hash=hash(string)
 		block={}
@@ -14,6 +14,25 @@ class CryptoChain:
 		block['merkle_root']=merkle_root
 		block['prevoius_hash']=prevoius_hash
 		block['current_hash']=current_hash
-	
 
+	def merkle_root(self,transactions):
+		hash_transactions=[hash(x) for x in transactions]
+		while(len(hash_transactions)!=1):
+			if len(hash_transactions)%2!=0:
+				last_transaction=hash_transactions[-1]
+				hash_transactions.append(hash_transactions)
+			transaction_s=[]
+			for i in range(0,len(hash_transactions),2):
+				transaction_s.append(hash(str(hash_transactions[i])+str(hash_transactions[i+1])))
+			hash_transactions=transaction_s
+		merkle=hash_transactions[0]
+		return merkle
+
+
+
+
+if __name__ == '__main__':
+	cc=CryptoChain()
+	lister=['ssss','ssssssssss','ssssssssssssssssssssss']
+	print(cc.merkle_root(lister))
 
