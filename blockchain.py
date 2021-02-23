ef1b2a697068aed6a885d0241b10660b72259181
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-23 13:20:06 +0530

    genesis block added

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,26 @@
 from utils import hash
+import time
+timestamp = str(time.time())
 
 class CryptoChain:
 	def __init__(self):
 		self.blocks=[]
 		self.max_nounce=100000000
 		self.pending_transactions=[]
+		self.current_block=1
+		transactions=[{
+		'transaction_id':'f8ef5338-540e-48c3-bb5c-97be5aceaf8b',
+		'timestamp':timestamp,
+		'sender':'Sai Durga Kamesh Kota',
+		'receiver':'ksdkamesh99',
+		'amount':'100'
+}]
+		prevoius_hash='0000'
+		self.block.append(create_block(1,transactions,prevoius_hash))
 
-	def create_block(self,index,transactions,nounce,prevoius_hash,merkle_root):
+	def create_block(self,index,transactions,prevoius_hash):
+		merkle_root=merkle_root(transactions)
+		nounce=mining(index,transactions,prevoius_hash,merkle_root)
 		string=str(index)+str(transactions)+str(nounce)+str(prevoius_hash)+str(merkle_root)
 		current_hash=hash(string)
 		block={}
@@ -19,7 +33,7 @@ class CryptoChain:
 		return block
 
 	def merkle_root(self,transactions):
-		hash_transactions=[hash(x) for x in transactions]
+		hash_transactions=[hash(str(x)) for x in transactions]
 		while(len(hash_transactions)!=1):
 			if len(hash_transactions)%2!=0:
 				last_transaction=hash_transactions[-1]
@@ -30,6 +44,7 @@ class CryptoChain:
 			hash_transactions=transaction_s
 		merkle=hash_transactions[0]
 		return merkle
+
 	def mining(self,index,transactions,prevoius_hash,merkle_root):
 		for i in range(1,self.max_nounce):
 			string=str(index)+str(transactions)+str(i)+str(prevoius_hash)+str(merkle_root)
@@ -37,8 +52,6 @@ class CryptoChain:
 			if current_hash.startswith('0000'):
 				return i 
 
-
-		
 
 
 
