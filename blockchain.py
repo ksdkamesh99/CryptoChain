3f5228ba1f9b1eb921551d446312967f4dabb3ad
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-23 12:40:47 +0530

    pending transactions added

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ from utils import hash
 class CryptoChain:
 	def __init__(self):
 		self.blocks=[]
+		self.max_nounce=100000000
+		self.pending_transactions=[]
 
 	def create_block(self,index,transactions,nounce,prevoius_hash,merkle_root):
 		string=str(index)+str(transactions)+str(nounce)+str(prevoius_hash)+str(merkle_root)
@@ -14,6 +16,7 @@ class CryptoChain:
 		block['merkle_root']=merkle_root
 		block['prevoius_hash']=prevoius_hash
 		block['current_hash']=current_hash
+		return block
 
 	def merkle_root(self,transactions):
 		hash_transactions=[hash(x) for x in transactions]
@@ -27,6 +30,15 @@ class CryptoChain:
 			hash_transactions=transaction_s
 		merkle=hash_transactions[0]
 		return merkle
+	def mining(self,index,transactions,prevoius_hash,merkle_root):
+		for i in range(1,self.max_nounce):
+			string=str(index)+str(transactions)+str(i)+str(prevoius_hash)+str(merkle_root)
+			current_hash=str(hash(string))
+			if current_hash.startswith('0000'):
+				return i 
+
+
+		
 
 
 
