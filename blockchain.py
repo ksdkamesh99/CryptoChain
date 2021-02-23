64517cc90e874fc2a596827b1aa12ae0eb7c59a9
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-02-23 09:49:57 +0530

    create block implementation added

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,19 @@
-from hashlib import sha256
-print(sha256("kamesh".encode('ascii')).hexdigest())
+from utils import hash
+class CryptoChain:
+	def __init__(self):
+		self.blocks=[]
+		self.genesis_block=create_block(1)
+
+	def create_block(self,index=1,transactions,nounce,prevoius_hash,merkle_root):
+		string=str(index)+str(transactions)+str(nounce)+str(prevoius_hash)+str(merkle_root)
+		current_hash=hash(string)
+		block={}
+		block['index']=index
+		block['transactions']=transactions
+		block['nounce']=nounce
+		block['merkle_root']=merkle_root
+		block['prevoius_hash']=prevoius_hash
+		block['current_hash']=current_hash
+	
+
+
